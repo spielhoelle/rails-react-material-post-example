592dd3bfb2f04175f694e9e3b3eb58ef3d2f7a6c
Thomas Kuhnert <tfkuhnert@gmail.com>
2017-07-28 14:33:22 +0200

    remove useless stuff

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -46,30 +46,6 @@ export default {
       exclude: /node_modules/,
       use: ['babel-loader']
     },
-      /*
-       * Sass loader (required for Bootstrap 4)
-       */
-      {
-        test: /\.css$/,
-        use: ['raw-loader']
-      },
-
-      {
-        test: /\.scss$/,
-        use: ['raw-loader', 'sass-loader']
-      },
-
-      /*
-       * Font loaders, required for font-awesome-sass-loader and bootstrap-loader
-       */
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader"
-      },
     ]
   },
   resolve: {
